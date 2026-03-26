9dfcf86fe61d29517f892be7605233fc6912aed2
manuelechavarria <maechavarriaor@gmail.com>
2026-03-26 08:33:31 -0500

    Ajustes CFA

--- src/config/global.js
+++ src/config/global.js
@@ -195,7 +195,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CFA2_12210015_DU.pdf',
       },
       {
         icono: 'fas fa-download',
--- src/config/titulo.js
+++ src/config/titulo.js
@@ -1,1 +1,1 @@
-module.exports = 'Ecored Base PKG'
+module.exports = 'Evaluación integral y plan de mejora del proyecto'
